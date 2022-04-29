443a2f0a66a3d1a3fa81b915316e8628d5ee04b7
Gustavo Guimarães Martins <90704921+GustavoGuimaraes32@users.noreply.github.com>
2022-04-29 16:30:48 -0300

    Modificando botão do card

--- hemocentros.php
+++ hemocentros.php
@@ -1,3 +1,12 @@
+<?php
+
+include('config.php');
+
+$Consulta = 'SELECT * FROM hemocentro';
+$con = $mysql->query($Consulta) or die ($mysql->error);
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -6,18 +15,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hemocentros</title>
     <link rel="stylesheet" href="css/style.css">
+    <script src="https://kit.fontawesome.com/93d18202e7.js" crossorigin="anonymous"></script>
 </head>
 <body>
+
     <div class="flex-box-card-container">
+        <?php while($dado = $con->fetch_array()){ ?>
         <div class="card-hemo">
             <img src="DSC03039.jpg" alt="">
-            <h2 class="title">HemoBanco</h2>
+            <h2 class="title"><?php echo $dado["Nome"]; ?></h2>
             <div class="endereco">
                 <i class="fa-solid fa-location-dot"></i>
-                <p>Localização: Curitiba</p>
+                <p>Localização: <?php echo $dado["Cidade"]; ?></p>
             </div>
             <button>Saiba Mais</button>
         </div>
+        <?php } ?>
     </div>
+    
 </body>
 </html>

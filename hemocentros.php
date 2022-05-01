f7aa2488cede848052d435aa3c0ca7b135f89880
Gustavo Guimarães Martins <90704921+GustavoGuimaraes32@users.noreply.github.com>
2022-05-01 15:55:11 -0300

    Adicionando Interface da Tela do Hemocentro e navbar nas páginas

--- hemocentros.php
+++ hemocentros.php
@@ -16,9 +16,48 @@ $result = $conexao->query($sql);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hemocentros</title>
     <link rel="stylesheet" href="css/style.css">
+    <script src="https://kit.fontawesome.com/65ea520fa5.js" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/93d18202e7.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="js/navbar.js"></script>
 </head>
 <body>
+
+    <nav>
+        <a href="#"><img src="img/logo.png" class="nav-logo"></a>
+
+        <!-- BOTAO PARA ABRIR O MENU -->
+        <div class="menu-btn">
+            <i class="fas fa-bars"></i>
+        </div>
+
+        <div class="side-bar">
+
+            <!-- BOTAO PARA FECHAR O MENU -->
+            <div class="close-btn">
+                <i class="fas fa-times"></i>
+            </div>
+
+            <div class="menu">
+                <div class="item"><a href="#"><i class="fas fa-desktop"></i>Home</a></div>
+                <div class="item"><a href="hemocentro.html"><i class="fa-solid fa-screwdriver-wrench"></i>Interface Tela Hemocentro</a></div>
+                <div class="item"><a href="hemocentros.php"><i class="fa-solid fa-house-chimney-medical"></i>Hemocentros</a></div>
+                <div class="item"><a href="#"><i class="fa-solid fa-building"></i>Sobre Nós</a></div>
+
+                <div class="item">
+                    <a class="sub-btn"><i class="fas fa-globe-americas"></i>Cadastro<i
+                            class="fas fa-angle-right dropdown"></i> </a>
+                    <div class="sub-menu">
+                        <a href="formulario.html" class="sub-item">Cadastro Hemocentro</a>
+                        <a href="#" class="sub-item">Cadastro Doador</a>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </nav>
 
     <div class="flex-box-card-container">
         <?php while($user_data = mysqli_fetch_assoc($result)) { ?>
---
+++ js/navbar.js
@@ -0,0 +1,22 @@
+$(document).ready(function () 
+{
+    //jquery for toggle sub menu
+    $('.sub-btn').click(function () {
+        $(this).next('.sub-menu').slideToggle();
+        $(this).find('.dropdown').toggleClass('rotate');
+    });
+
+    //jquery para expandir e recolher o menu
+    $('.menu-btn').click(function () {
+        $('.side-bar').addClass('active');
+        $('.menu-btn').css("visibility", "hidden");
+    });
+
+    $('.close-btn').click(function () {
+        $('.side-bar').removeClass('active');
+        $('.menu-btn').css("visibility", "visible");
+    });
+
+    fLocalCardsHospedagens();
+
+});

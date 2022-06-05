179e07c177265dcf22bda5c3894acaa6f17f5290
Gustavo Guimarães Martins <90704921+GustavoGuimaraes32@users.noreply.github.com>
2022-06-05 13:59:02 -0300

    Estilo Criar Agendamento

--- agendaHemocentro.php
+++ agendaHemocentro.php
@@ -6,7 +6,6 @@
     if(isset($_GET['CodHemocentro']))
     {
         $CodHemocentro = $_GET['CodHemocentro'];
-        print_r($CodHemocentro);
     }
     
 ?>
@@ -18,19 +17,73 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Criar Agenda</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
+    <script src="https://kit.fontawesome.com/65ea520fa5.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/93d18202e7.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="js/navbar.js"></script>
 </head>
 <body>
 
-    <form action="criaAgenda.php" method="POST">
-        <input type="date" name="DataAgenda" id="DataAgenda">
-        <input type="time" name="HoraAgenda" id="HoraAgenda">
+    <nav>
+        <a href="#"><img src="img/logo.png" class="nav-logo"></a>
 
-        <div class="submitAgenda">
-            <input type="hidden" name="CodHemocentro" value="<?php echo $CodHemocentro?>">
-            <input type="submit" name="create" id="create" value="Criar Agenda">
+        <!-- BOTAO PARA ABRIR O MENU -->
+        <div class="menu-btn">
+            <i class="fas fa-bars"></i>
         </div>
+
+        <div class="side-bar">
+
+            <!-- BOTAO PARA FECHAR O MENU -->
+            <div class="close-btn">
+                <i class="fas fa-times"></i>
+            </div>
+
+            <div class="menu">
+                <div class="item"><a href="#"><i class="fas fa-desktop"></i>Home</a></div>
+                <div class="item"><a href="hemocentros.php"><i class="fa-solid fa-house-chimney-medical"></i>Hemocentros</a></div>
+                <div class="item"><a href="#"><i class="fa-solid fa-building"></i>Sobre Nós</a></div>
+                <div class="item"><a href="perfilDoador.php"><i class="fa-solid fa-user"></i>Meu Perfil</a></div>
+
+                <div class="item">
+                    <a class="sub-btn"><i class="fa-solid fa-right-to-bracket"></i>Cadastro<i
+                            class="fas fa-angle-right dropdown"></i> </a>
+                    <div class="sub-menu">
+                        <a href="formulario.html" class="sub-item">Cadastro Hemocentro</a>
+                        <a href="formularioDoador.php" class="sub-item">Cadastro Doador</a>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </nav>
+
+
+    <div class="criarAgenda-container">
+        <!--
+        <div class="criarAgenda-text">
+            <h1>Criar Agenda</h1>
+            <p>Para criar uma horário para agendamento basta seleciona uma data e uma horário e clicar no botão Criar Agenda!</p>
+        </div>
+        -->
+        <div class="form-criarAgenda-container">
+            <form action="criaAgenda.php" method="POST">
+                <div class="input-container">
+                    <input type="date" class="inputAgenda" name="DataAgenda" id="DataAgenda">
+                    <input type="time" class="inputAgenda" name="HoraAgenda" id="HoraAgenda">
         
-    </form>
+                    <div class="submitAgenda">
+                        <input type="hidden" name="CodHemocentro" value="<?php echo $CodHemocentro?>">
+                        <input type="submit" name="create" id="create" value="Criar Agenda">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
     
 </body>
 </html>

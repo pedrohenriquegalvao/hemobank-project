9370bb4a3628f1d3b174f1cbce85798abec7a30f
alvessleo <90728273+alvessleo@users.noreply.github.com>
2022-06-06 08:30:24 -0300

    Estilizando editar perfil do doador

--- editDoador.php
+++ editDoador.php
@@ -31,23 +31,73 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>form</title>
+    <title>Editar Perfil</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
+    <script src="https://kit.fontawesome.com/65ea520fa5.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/93d18202e7.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="js/navbar.js"></script>
 </head>
 <body>
-    <div class="form-editar-doador">
-        <form action="saveEditDoador.php" method="POST">
-            <label for="nomeDoador">Nome:</label><br>
-            <input type="text" id="nomeDoador" name="nomeDoador" value="<?php echo $nomeDoador?>"><br>
-            <label for="cpfDoador">CPF:</label><br>
-            <input type="text" id="cpfDoador" name="cpfDoador" value="<?php echo $cpfDoador?>"><br>
-            <label for="dataNascDoador">Data de Nascimento:</label><br>
-            <input type="text" id="dataNascDoador" name="dataNascDoador" value="<?php echo $dataNascDoador?>"><br>
-            <label for="emailDoador">Email:</label><br>
-            <input type="text" id="emailDoador" name="emailDoador" value="<?php echo $emailDoador?>"><br>
-            <input type="hidden" name="codDoador" value="<?php echo $codDoador ?>">
-            <input type="submit" name="update" id="update" value="Salvar alterações">
-        </form>
+    <nav>
+        <a href="#"><img src="img/logo.png" class="nav-logo"></a>
+
+        <!-- BOTAO PARA ABRIR O MENU -->
+        <div class="menu-btn">
+            <i class="fas fa-bars"></i>
+        </div>
+
+        <div class="side-bar">
+
+            <!-- BOTAO PARA FECHAR O MENU -->
+            <div class="close-btn">
+                <i class="fas fa-times"></i>
+            </div>
+
+            <div class="menu">
+                <div class="item"><a href="#"><i class="fas fa-desktop"></i>Home</a></div>
+                <div class="item"><a href="hemocentros.php"><i class="fa-solid fa-house-chimney-medical"></i>Hemocentros</a></div>
+                <div class="item"><a href="#"><i class="fa-solid fa-building"></i>Sobre Nós</a></div>
+                <div class="item"><a href="perfilDoador.php"><i class="fa-solid fa-user"></i>Meu Perfil</a></div>
+
+                <div class="item">
+                    <a class="sub-btn"><i class="fa-solid fa-right-to-bracket"></i></i>Cadastro<i
+                            class="fas fa-angle-right dropdown"></i> </a>
+                    <div class="sub-menu">
+                        <a href="formulario.html" class="sub-item">Cadastro Hemocentro</a>
+                        <a href="formularioDoador.php" class="sub-item">Cadastro Doador</a>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </nav>
+    <div class="wrapper">
+        <div class="left">
+            <img src="profile-pic-animate.svg" 
+            alt="user">
+            <p class="type-usuario">DOADOR</p>
+        </div>
+        <div class="right">
+        <div class="form-editar-doador">
+            <form action="saveEditDoador.php" method="POST">
+                <label for="nomeDoador">Nome:</label><br>
+                <input type="text" id="nomeDoador" name="nomeDoador" value="<?php echo $nomeDoador?>"><br>
+                <label for="cpfDoador">CPF:</label><br>
+                <input type="text" id="cpfDoador" name="cpfDoador" value="<?php echo $cpfDoador?>"><br>
+                <label for="dataNascDoador">Data de Nascimento:</label><br>
+                <input type="text" id="dataNascDoador" name="dataNascDoador" value="<?php echo $dataNascDoador?>"><br>
+                <label for="emailDoador">Email:</label><br>
+                <input type="text" id="emailDoador" name="emailDoador" value="<?php echo $emailDoador?>"><br>
+                <input type="hidden" name="codDoador" value="<?php echo $codDoador ?>">
+                <input type="submit" name="submit" id="submit" value="Salvar alterações">
+            </form>
+        </div>          
+        </div>
+        </div>
     </div>
 </body>
 </html>
